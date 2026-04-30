3bd9d1a43341bc722dcf06e5422f0fa8e6b5d3ad
browa <to.poncez@duocuc.cl>
2026-04-30 18:54:33 -0400

    first commit

---
+++ reporte-service/src/test/java/com/fullstack/reporte_service/ReporteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack.reporte_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReporteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
