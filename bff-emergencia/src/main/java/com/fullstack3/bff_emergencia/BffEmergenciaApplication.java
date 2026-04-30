8ca3ffae5440ac128297b5c1966b31d41bb0dad8
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-04-30 19:47:52 -0400

    first commit

---
+++ bff-emergencia/src/main/java/com/fullstack3/bff_emergencia/BffEmergenciaApplication.java
@@ -0,0 +1,13 @@
+package com.fullstack3.bff_emergencia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BffEmergenciaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BffEmergenciaApplication.class, args);
+	}
+
+}
