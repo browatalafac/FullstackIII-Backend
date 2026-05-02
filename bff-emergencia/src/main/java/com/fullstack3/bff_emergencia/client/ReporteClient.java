85d811aab99277cb4514c0b67f17662c3f0678ca
UlisinBonbin <74909200+UlisinBonbin@users.noreply.github.com>
2026-05-02 16:52:48 -0400

    Se agregaron los metodos que se agregaron en el microservicio reporte al bff

--- bff-emergencia/src/main/java/com/fullstack3/bff_emergencia/client/ReporteClient.java
+++ bff-emergencia/src/main/java/com/fullstack3/bff_emergencia/client/ReporteClient.java
@@ -3,11 +3,21 @@ package com.fullstack3.bff_emergencia.client;
 import com.fullstack3.bff_emergencia.dto.ReporteRequestDTO;
 import com.fullstack3.bff_emergencia.dto.ReporteResponseDTO;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
 
 @FeignClient(name = "reporte-service", url = "http://localhost:8081/api/v1/reportes")
 public interface ReporteClient {
     @PostMapping
     ReporteResponseDTO guardarReporte(@RequestBody ReporteRequestDTO request);
+
+    @GetMapping
+    ReporteResponseDTO getById(@PathVariable("id") Long id);
+
+    @GetMapping
+    List<ReporteResponseDTO> getAllReportes();
 }
