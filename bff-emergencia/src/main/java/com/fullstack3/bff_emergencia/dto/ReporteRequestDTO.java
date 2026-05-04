30aa34bdd15f127720a3ce3579da45cab0e4c494
MP-Alumno <MP-Alumno@duoc.acad>
2026-05-04 09:47:07 -0400

    Se completo el bff. agreando la clase ResilienteClientService. añadiendo las configuraciones del cricuit breacker y añadiendo importaciones y anotaciones en otras clases

--- bff-emergencia/src/main/java/com/fullstack3/bff_emergencia/dto/ReporteRequestDTO.java
+++ bff-emergencia/src/main/java/com/fullstack3/bff_emergencia/dto/ReporteRequestDTO.java
@@ -1,9 +1,11 @@
 package com.fullstack3.bff_emergencia.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- bff-emergencia/src/main/java/com/fullstack3/bff_emergencia/dto/ReporteResponseDTO.java
+++ bff-emergencia/src/main/java/com/fullstack3/bff_emergencia/dto/ReporteResponseDTO.java
@@ -1,11 +1,13 @@
 package com.fullstack3.bff_emergencia.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@Builder
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
